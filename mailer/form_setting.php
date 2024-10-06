b718d482e64f7da0cac64d24245ea498f23a3baf
MuuAl0ne <sadokdev@proton.me>
2024-10-06 01:25:57 +0100

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Nouveau message de votre site 'Dr. Jean-Jacques GODEME - Mathématiques Appliquées | Optimisation & Traitement du Signal'"; //letter subject
+	$to = 'sadokgodeme@gmail.com'; // Enter Your E-mail
+	$from = 'admin@jjgodeme.com'; // Admin e-mail
+	$fromName = 'Jean-Jacques GODEME'; // Your company name
+	$charset = 'UTF-8';
+?>
